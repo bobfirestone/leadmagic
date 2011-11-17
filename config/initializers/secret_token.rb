626d4dd28dfbb98c7808fad17ffe5fdb685ebe22
Bob Firestone <bob@rbf.me>
2011-11-17 11:21:40 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leadmagic::Application.config.secret_token = 'd70daa7e51440bb0f3ec3047220e9fc660e87f5e41efa69d8944787aba4696aa8a6047ccf5f4903a21bafd95cbd3109dacbaeeae205c44700600703b6addb591'
