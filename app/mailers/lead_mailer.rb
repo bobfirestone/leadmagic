70e13a4acd61e685ceecf8148a7ffffdffb87361
Bob Firestone <bob@rbf.me>
2011-11-30 14:04:20 -0800

    added fields to campaign

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -3,12 +3,21 @@ class LeadMailer < ActionMailer::Base
   
   def new_lead(lead)
     @lead = lead
-    mail(:to => "#{lead.campaign.email}", :cc => "info@mainstreetchamber.net", :subject => "A new lead for you...")
+    if lead.campaign.cc.nil?
+      mail(:to => "#{lead.campaign.email}", :cc => "info@mainstreetchamber.net", :subject => "A new lead for you...")
+    else
+      mail(:to => "#{lead.campaign.email}", :cc => ["info@mainstreetchamber.net", "#{lead.campaign.cc}"], :subject => "A new lead for you...")
+    end
+
   end
   
   def welcome_email(lead)
     @lead = lead
-    mail(:to => "#{lead.firstname} #{lead.lastname} <#{lead.email}>", :subject => "#{lead.campaign.email_subject}")
+    if lead.campaign.cc.nil?
+      mail(:to => "#{lead.firstname} #{lead.lastname} <#{lead.email}>", :subject => "#{lead.campaign.email_subject}")
+    else
+      mail(:to => "#{lead.firstname} #{lead.lastname} <#{lead.email}>", :cc => "#{lead.campaign.cc}", :subject => "#{lead.campaign.email_subject}")
+    end
   end
   
 end
